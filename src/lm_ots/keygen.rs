60bad60a26439aff4a291d8057b5d72f1bc6f994
Maurice Hieronymus <maurice_hieronymus@yahoo.de>
2021-05-16 22:00:02 +0200

    Add macro for public key implementation generation

--- src/lm_ots/keygen.rs
+++ src/lm_ots/keygen.rs
@@ -37,56 +37,68 @@ macro_rules! generate_private_key_gen_impl {
     };
 }
 
-impl PublicKey<LMOTS_SHA256_N32_W1_PUBLIC_KEY> {
-    pub fn generate(private_key: &PrivateKey<LMOTS_SHA256_N32_W1_PRIVATE_KEY>) -> Self {
-        let I = private_key.I;
-        let q = private_key.q;
-
-        const p: usize = LMOTS_SHA256_N32_W1::p;
-        const w: usize = LMOTS_SHA256_N32_W1::w;
-        const n: usize = LMOTS_SHA256_N32_W1::n;
-
-        let max_word_index = (1 << w) - 1;
-
-        let key = private_key.key.get_private_key(); 
-        let mut hasher = Sha256::default();
-
-        let mut y = [[0_u8; n]; p];
-
-        for i in 0..p {
-            let mut tmp = key[i];
+macro_rules! generate_public_key_gen_impl {
+    ($name: ident) => {
+        paste! {
+            impl PublicKey<[<$name _PUBLIC_KEY>]> {
+                pub fn generate(private_key: &PrivateKey<[<$name _PRIVATE_KEY>]>) -> Self {
+                    let I = private_key.I;
+                    let q = private_key.q;
             
-            for j in 0..max_word_index {
-                hasher.update(&I);
-                hasher.update(&q);
-                hasher.update(&u16str(i as u16));
-                hasher.update(&u8str(j as u8));
-                hasher.update(&tmp);
-
-                tmp = hasher.finalize_reset().into();
+                    const p: usize = $name::p;
+                    const w: usize = $name::w;
+                    const n: usize = $name::n;
+            
+                    let max_word_index = (1 << w) - 1;
+            
+                    let key = private_key.key.get_private_key(); 
+                    let mut hasher = Sha256::default();
+            
+                    let mut y = [[0_u8; n]; p];
+            
+                    for i in 0..p {
+                        let mut tmp = key[i];
+                        
+                        for j in 0..max_word_index {
+                            hasher.update(&I);
+                            hasher.update(&q);
+                            hasher.update(&u16str(i as u16));
+                            hasher.update(&u8str(j as u8));
+                            hasher.update(&tmp);
+            
+                            tmp = hasher.finalize_reset().into();
+                        }
+                        y[i] = tmp;
+                    }
+            
+                    hasher.update(&I);
+                    hasher.update(&q);
+                    hasher.update(&D_PBLC);
+                    
+                    for item in y.iter() {
+                        hasher.update(item);
+                    }
+            
+                    let K: [u8; n] = hasher.finalize().into();
+            
+                    PublicKey {
+                        I,
+                        q,
+                        key: [<$name _PUBLIC_KEY>]::create(K)
+                    }
+                }
             }
-            y[i] = tmp;
+            
         }
-
-        hasher.update(&I);
-        hasher.update(&q);
-        hasher.update(&D_PBLC);
-        
-        for item in y.iter() {
-            hasher.update(item);
-        }
-
-        let K: [u8; n] = hasher.finalize().into();
-
-        PublicKey {
-            I,
-            q,
-            key: LMOTS_SHA256_N32_W1_PUBLIC_KEY::create(K)
-        }
-    }
+    };
 }
 
 generate_private_key_gen_impl!(LMOTS_SHA256_N32_W1);
 generate_private_key_gen_impl!(LMOTS_SHA256_N32_W2);
 generate_private_key_gen_impl!(LMOTS_SHA256_N32_W4);
 generate_private_key_gen_impl!(LMOTS_SHA256_N32_W8);
+
+generate_public_key_gen_impl!(LMOTS_SHA256_N32_W1);
+generate_public_key_gen_impl!(LMOTS_SHA256_N32_W2);
+generate_public_key_gen_impl!(LMOTS_SHA256_N32_W4);
+generate_public_key_gen_impl!(LMOTS_SHA256_N32_W8);

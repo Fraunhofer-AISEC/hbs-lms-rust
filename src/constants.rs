2711d562b4b85b79727bfff5ace04d6624af2d4b
Maurice Hieronymus <maurice_hieronymus@yahoo.de>
2021-07-15 13:35:08 +0200

    Allow W=1

--- src/constants.rs
+++ src/constants.rs
@@ -10,7 +10,7 @@ pub const D_LEAF: [u8; 2] = [0x82, 0x82];
 pub const D_INTR: [u8; 2] = [0x83, 0x83];
 
 pub const MAX_N: usize = 32;
-pub const MAX_P: usize = 34;
+pub const MAX_P: usize = 265;
 
 pub const MAX_M: usize = 32;
 pub const MAX_H: usize = 5;

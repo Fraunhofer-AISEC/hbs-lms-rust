8fb360d4855c1284e26b726d5f4e870078967c68
Maurice Hieronymus <maurice_hieronymus@yahoo.de>
2021-08-05 11:15:01 +0200

    Add function to retrieve the public key

--- src/lib.rs
+++ src/lib.rs
@@ -8,17 +8,26 @@ mod lm_ots;
 mod lms;
 mod util;
 
+use hasher::Hasher;
+use hss::definitions::HssPrivateKey;
+use hss::rfc_private_key::RfcPrivateKey;
+
 pub use crate::constants::Seed;
 pub use crate::hasher::sha256::Sha256Hasher;
 pub use crate::hss::parameter::HssParameter;
 pub use crate::lm_ots::parameters::*;
 pub use crate::lms::parameters::*;
 
+pub use crate::hss::definitions::HssPublicKey;
 pub use crate::hss::definitions::InMemoryHssPublicKey;
 pub use crate::hss::signing::InMemoryHssSignature;
-pub use crate::hss::definitions::HssPublicKey;
-pub use crate::hss::rfc_private_key::RfcPrivateKey;
 
 pub use crate::hss::hss_keygen;
 pub use crate::hss::hss_sign;
 pub use crate::hss::hss_verify;
+
+pub fn get_public_key<H: Hasher>(private_key: &[u8]) -> Option<HssPublicKey<H>> {
+    let rfc_private_key = RfcPrivateKey::<H>::from_binary_representation(private_key)?;
+    let hss_private_key = HssPrivateKey::from(&rfc_private_key, None).ok()?;
+    Some(hss_private_key.get_public_key())
+}

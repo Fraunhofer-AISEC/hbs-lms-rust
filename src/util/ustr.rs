befd6e2e0ed38a58628e3c576581562f7df16135
Maurice Hieronymus <maurice_hieronymus@yahoo.de>
2021-10-08 10:14:55 +0200

    Remove unused u8str function

--- src/util/ustr.rs
+++ src/util/ustr.rs
@@ -21,7 +21,3 @@ pub fn str64u(x: &[u8]) -> u64 {
 pub fn u16str(x: u16) -> [u8; 2] {
     x.to_be_bytes()
 }
-
-pub fn u8str(x: u8) -> [u8; 1] { // TODO wird das noch benötigt?
-    x.to_be_bytes()
-}

4e391874c17fafbb8dd81f9b89067d08d7751c9c
Alexander Wagner <alexander.wagner@aisec.fraunhofer.de>
2021-12-03 10:04:06 +0100

    util/helper: Add read()

--- src/util/helper.rs
+++ src/util/helper.rs
@@ -2,8 +2,12 @@ pub fn is_odd(x: usize) -> bool {
     x % 2 == 1
 }
 
+pub fn read<'a>(src: &'a [u8], length: usize, index: &usize) -> &'a [u8] {
+    &src[*index..*index + length]
+}
+
 pub fn read_and_advance<'a>(src: &'a [u8], length: usize, index: &mut usize) -> &'a [u8] {
-    let result = &src[*index..*index + length];
+    let result = read(src, length, index);
     *index += length;
     result
 }

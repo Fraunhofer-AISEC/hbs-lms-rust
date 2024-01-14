bc1a178a9e25e48b1bf2bb1176f992b3bfeddd26
Stephan Schwarzmann <stephan@schwarzmann-web.de>
2024-01-14 20:12:11 +0100

    sst: use constant, tag unused symbols (_)

--- src/sst/sign.rs
+++ src/sst/sign.rs
@@ -1,14 +1,14 @@
 use crate::signature::Error;
 use crate::constants::Node;
+use crate::constants::MAX_HASH_SIZE;
 use crate::hss::signing::HssSignature;
 use crate::HashChain;
 use tinyvec::ArrayVec;
 
 pub struct SstsSignature<H: HashChain> {
     pub hss_signature: HssSignature<H>,
-    // use an ArrayVec with a @TODO max size?
     pub our_node: Node,
-    pub auth_path: ArrayVec<[Node; 32]>, // order: sort on-the-fly dep. on Node's values?
+    pub auth_path: ArrayVec<[Node; MAX_HASH_SIZE]>, // order: sort on-the-fly dep. on Node's values?
 }
 
 pub fn sign<H: HashChain>(
